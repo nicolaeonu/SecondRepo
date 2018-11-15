5a966bba79bfbb1260d0d8fc00bbed4e3f6a4ee8
Nicolae <44190853+nicolaeonu@users.noreply.github.com>
2018-11-15 11:53:53 +0200

    Add files via upload

---
+++ Arrays/src/com/nicolae/Main.java
@@ -0,0 +1,67 @@
+package com.nicolae;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int[] theIntegers = getIntegers(5);
+        int[] sorted = sortIntegers(theIntegers);
+        printArray(sorted);
+    }
+
+    public static int[] getIntegers(int number){
+        int[] array = new int[number];
+        System.out.println("Enter " +number + " integer vaues.\r");
+
+        for(int i=0; i < array.length; i ++){
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
+    public static void printArray(int[] array){
+        for(int i=0; i< array.length;i++){
+            System.out.println("Element " + i + " contents "+ array[i]);
+        }
+    }
+
+    public static int[] sortIntegers(int[] array){
+//        int[] sortedArray= new int [array.length];
+//        for(int i = 0; i <array.length; i ++){
+//            sortedArray[i]= array[i];
+//        }
+        int[] sortedArray = Arrays.copyOf(array, array.length);
+
+        boolean flag = true;
+        int temp;
+        while(flag){
+            flag= false;
+
+            for(int i = 0; i < sortedArray.length-1; i++){
+                if(sortedArray[i]< sortedArray[i+1]){
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i+1];
+                    sortedArray[i+1] = temp;
+                    flag = true;
+                }
+            }
+        }
+        return sortedArray;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
